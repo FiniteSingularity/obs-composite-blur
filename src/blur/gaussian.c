ddec85c99d3d9aeefb7eb4a7a46c10cfa19835e4
FiniteSingularity <finitesingularityttv@gmail.com>
2025-02-24 14:20:27 -0600

    Test int with 1

--- src/blur/gaussian.c
+++ src/blur/gaussian.c
@@ -573,7 +573,9 @@ static void gaussian_vector_smooth_gradient(composite_blur_filter_data_t* data)
 	if (!data->vb_smoothed_gradient) {
 		data->vb_smoothed_gradient = gs_texrender_create(GS_RGBA, GS_ZS_NONE);
 	}
-	data->kawase_passes = (int)(data->vector_blur_smoothing + 1.0f);
+	//data->kawase_passes = (int)(data->vector_blur_smoothing + 1.0f);
+	data->kawase_passes = 1;
+
 
 	gs_texrender_t* tmp = data->input_texrender;
 	data->input_texrender = data->vb_gradient;
