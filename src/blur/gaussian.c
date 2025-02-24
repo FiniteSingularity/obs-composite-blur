9ddb1f7c827618932f2296b57a2e84ef3dca429c
FiniteSingularity <finitesingularityttv@gmail.com>
2025-02-24 14:45:47 -0600

    Disable warning

--- src/blur/gaussian.c
+++ src/blur/gaussian.c
@@ -573,9 +573,8 @@ static void gaussian_vector_smooth_gradient(composite_blur_filter_data_t* data)
 	if (!data->vb_smoothed_gradient) {
 		data->vb_smoothed_gradient = gs_texrender_create(GS_RGBA, GS_ZS_NONE);
 	}
-	data->kawase_passes = (int)(roundf(data->vector_blur_smoothing)) + 1;
-	//data->kawase_passes = 1;
-
+#pragma warning(suppress: 4244)
+	data->kawase_passes = (int)(data->vector_blur_smoothing) + 1;
 
 	gs_texrender_t* tmp = data->input_texrender;
 	data->input_texrender = data->vb_gradient;
